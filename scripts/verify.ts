be7ca7a1aa526f85e1f45af1d059314d8fe5885e
Bhavin <bhavin.aggarwal@antiersolutions.com>
2022-07-06 17:40:22 +0530

    MasterChef PostAudit

--- scripts/verify.ts
+++ scripts/verify.ts
@@ -8,11 +8,11 @@ async function main() {
   //   });
 
   await hre.run("verify:verify", {
-    address: "0x85EB04E60ee5920eaFfe2807CcC1521358F6Cf5D",
+    address: "0x642A34f580FBA0D9b82ae8caD9112aAf36B34c1A",
     constructorArguments: [
-      "0x6410285e47A98D5885169CB1f120BA976724C370",
+      "0x0eD81CAe766d5B1a4B3ed4DFbED036be13c6C09C",
       "0xF0360dA6bE15f586D2d673d11323929bf4205D3f",
-      "10889229",
+      "10930109",
       "30000000000",
     ],
     contract: "contracts/Masterchef.sol:MasterChef",
